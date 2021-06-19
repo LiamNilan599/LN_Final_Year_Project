499a9be94d578086a46390672319fac78c5ab948
LiamNilan599 <68008677+LiamNilan599@users.noreply.github.com>
2021-06-19 15:10:09 +0100

    Add files via upload

---
+++ ESP_bootstrap.ino
@@ -0,0 +1,57 @@
+#include "WiFi.h"
+#include "SPIFFS.h"
+#include "ESPAsyncWebServer.h"
+ 
+const char* ssid = "TP-LINK_7CEE";
+const char* password = "73669722";
+
+const char* http_username = "admin";
+const char* http_password = "admin";
+ 
+AsyncWebServer server(80);
+ 
+void setup(){
+  Serial.begin(115200);
+ 
+  if(!SPIFFS.begin()){
+     Serial.println("An Error has occurred while mounting SPIFFS");
+     return;
+  }
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+ 
+  Serial.println(WiFi.localIP());
+ 
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    if(!request->authenticate(http_username, http_password))
+      return request->requestAuthentication();
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+  
+  server.on("/logout", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(401);
+  });
+  server.on("/logged-out", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/logged_out.html", "text/html");
+  });
+
+  server.on("/src/bootstrap.bundle.min.js", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/src/bootstrap.bundle.min.js", "text/javascript");
+  });
+ 
+  server.on("/src/jquery-3.3.1.min.js", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/src/jquery-3.3.1.min.js", "text/javascript");
+  });
+ 
+  server.on("/src/bootstrap.min.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/src/bootstrap.min.css", "text/css");
+  });
+  server.begin();
+}
+ 
+void loop(){}
