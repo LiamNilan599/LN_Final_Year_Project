84badbd75ff44b6365d37d76f286fd9cb684c291
Liam Nilan <g00366621@gmit.ie>
2021-10-05 10:39:27 +0100

    Modal Verification

--- src/Components/Layout/NewEmployeeModal.js
+++ src/Components/Layout/NewEmployeeModal.js
@@ -1,7 +1,5 @@
 import { useRef } from 'react';
 function NewEmployeeModal(props) {
-
-    
     function cancelHandler() {
         props.onCancel();
     }
@@ -21,7 +19,45 @@ function NewEmployeeModal(props) {
         const enteredPpsn = ppsnInputRef.current.value;
         const enteredWage = wageInputRef.current.value;
 
-        
+    var letterRegex = new RegExp(/^[a-zA-Z]+$/);
+    if (enteredName.match(letterRegex) === null) {
+        alert("You must only enter letters in the Name field");
+        return false;
+      }
+    else if (enteredAge === "" || enteredAge === null) {
+        alert("Age must be filled out");
+        return false;
+      }
+    else if (enteredRole.match(letterRegex) === null) {
+        alert("You must only enter letters in the Role field");
+        return false;
+      }
+    else if (enteredPpsn.length < 8) {
+        alert("PPSN must be eight digits");
+        return false;
+    }
+    else if (enteredAge < 18 && enteredWage < 7.14)
+      {
+        alert("Wage must be at least €7.14");
+        return false;
+      }
+    else if (enteredAge === 18 && enteredWage < 8.16)
+      {
+        alert("Wage must be at least €8.16");
+        return false;
+      }
+    else if (enteredAge === 19 && enteredWage < 9.18)
+      {
+        alert("Wage must be at least €9.18");
+        return false;
+      }
+    else if (enteredAge >= 20 && enteredWage < 10.2)
+      {
+        alert("Wage must be at least €10.2");
+        return false;
+      }
+    else
+      {
         const employeeData = {
             name: enteredName,
             age: enteredAge,
@@ -32,30 +68,9 @@ function NewEmployeeModal(props) {
 
         console.log(employeeData);
         props.onAddEmployee(employeeData);
+      }
     }
     return (
-        // <div className='modal'>
-        //     <div id="empModal" className="modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-
-        //     {/* <!-- Modal content --> */}
-        //         <div class="modal-content">
-        //             <div class="modal-header">
-        //                 <h5 class="modal-title">New Employee</h5>
-        //                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-        //                     <span aria-hidden="true">&times;</span>
-        //                 </button>
-        //             </div>
-        //             <form name="empForm" action="/get" target="hidden-form" onsubmit="return validateForm()">
-        //                 Name: <input type="text" name="inputString"></input>
-        //                 Age: <input type="number " name="inputAge"></input>
-        //                 PPSN: <input type="number " name="inputInt" maxlength = "8"></input>
-        //                 Hourly Wage: <input type="number " name="inputFloat"></input>
-        //                 <input style="display:none;" class="empId" type="hidden" id="empId" name="line" value=""></input>
-        //                 <input type="submit" value="Submit"></input>
-        //             </form>
-        //         </div>
-        //     </div>
-        // </div>
         <div>
             <div className='modal'>
                 <h3> New Employee </h3>
@@ -74,7 +89,7 @@ function NewEmployeeModal(props) {
                     </div>
                     <div className='control'>
                         <label htmlFor='ppsn'>PPSN</label>
-                        <input type='text' required id='ppsn' maxlength = "8" ref={ppsnInputRef} />
+                        <input type='text' required id='ppsn' maxLength = "8" ref={ppsnInputRef} />
                     </div>
                     <div className='control'>
                         <label htmlFor='wage'>Hourly Wage</label>
@@ -85,7 +100,7 @@ function NewEmployeeModal(props) {
                         <button className='btn' onClick={cancelHandler}>Cancel</button>
                     </div>
                 </form>
-                
+
             </div>
         </div>
     );
