cb3352e93a036223b9e5e5e5b24aa5bd7c672971
Liam Nilan <g00366621@gmit.ie>
2022-02-05 12:28:52 +0000

    Mongoose Implementation in node js

--- node/TestModule.js
+++
@@ -1,7 +0,0 @@
-function testFuncntion()
-{
-    console.log('Test Function Called');
-}
-var ThisString = "String";
-
-module.exports.testFuncntion = testFuncntion;
--- node/routes/index.js
+++ node/routes/index.js
@@ -1,41 +1,32 @@
- //Code is from the following tutorial: Node.js + Express - Tutorial - Insert and Get Data with MongoDB. Code source: https://github.com/mschwarzmueller/nodejs-basics-tutorial
+//Code is from the following tutorial: Node.js + Express - Tutorial - Insert and Get Data with MongoDB. Code source: https://github.com/mschwarzmueller/nodejs-basics-tutorial
 var express = require('express');
 var router = express.Router();
-var mongo = require('mongodb').MongoClient;
-var objectId = require('mongodb').ObjectID;
-var assert = require('assert');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test');
 
-var url = 'mongodb://localhost:27017/test';
+var Schema = mongoose.Schema;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+var EmployeeSchema = new Schema({
+  name: String,
+  age: Number,
+  role: String,
+  ppsn: String,
+  wage: Number
+}, { collection: 'user-data' });
+
+var EmployeeData = mongoose.model('EmployeeData', EmployeeSchema);
+
+router.get('/', function (req, res, next) {
   res.render('index');
 });
 
-router.get('/get-data', function(req, res, next) {
-  var resultArray = [];
-  mongo.connect(url, function(err, db) {
-    assert.equal(null, err);
-    var cursor = db.collection('user-data').find({}).toArray(function (err, result) {
-      if (err) throw err;
-      res.json(result)});
-    });
-  //   cursor.forEach(function(doc, err) {
-  //     assert.equal(null, err);
-  //     resultArray.push(doc);
-  //   }, function() {
-  //     db.close();
-  //     res.render('index', {items: resultArray});
-  //   });
-  // });
+router.get('/get-data', function (req, res, next) {
+  EmployeeData.find().lean().exec(function (err, doc) {
+    res.json(doc);
+  });
 });
 
-router.post('/insert', function(req, res, next) {
-  // var item = {
-  //   title: req.body.title,
-  //   content: req.body.content,
-  //   author: req.body.author
-  // };
+router.post('/insert', function (req, res, next) {
   var item = {
     name: req.body.name,
     age: req.body.age,
@@ -44,53 +35,32 @@ router.post('/insert', function(req, res, next) {
     wage: req.body.wage
   };
 
-  mongo.connect(url, function(err, db) {
-    assert.equal(null, err);
-    db.collection('user-data').insertOne(item, function(err, result) {
-      assert.equal(null, err);
-      console.log('Item inserted');
-      db.close();
-    });
-  });
+  var data = new EmployeeData(item);
+  data.save();
 
   res.redirect('/');
 });
 
-router.post('/update', function(req, res, next) {
-  // var item = {
-  //   title: req.body.title,
-  //   content: req.body.content,
-  //   author: req.body.author
-  // };
-  var item = {
-    name: req.body.name,
-    age: req.body.age,
-    role: req.body.role,
-    ppsn: req.body.ppsn,
-    wage: req.body.wage
-  };
+router.post('/update', function (req, res, next) {
   var id = req.body.id;
-  console.log(req.body);
-  mongo.connect(url, function(err, db) {
-    assert.equal(null, err);
-    db.collection('user-data').updateOne({"_id": objectId(id)}, {$set: item}, function(err, result) {
-      assert.equal(null, err);
-      console.log('Item updated');
-      db.close();
-    });
-  });
+  EmployeeData.findById(id, function (err, doc) {
+    if (err) {
+      console.error('error, no entry found');
+    }
+    doc.name = req.body.name
+    doc.age = req.body.age
+    doc.role = req.body.role
+    doc.ppsn = req.body.ppsn
+    doc.wage = req.body.wage
+    doc.save();
+  })
+  res.redirect('/');
 });
 
-router.post('/delete', function(req, res, next) {
+router.post('/delete', function (req, res, next) {
   var id = req.body.id;
-  mongo.connect(url, function(err, db) {
-    assert.equal(null, err);
-    db.collection('user-data').deleteOne({"_id": objectId(id)}, function(err, result) {
-      assert.equal(null, err);
-      console.log('Item deleted');
-      db.close();
-    });
-  });
+  EmployeeData.findByIdAndRemove(id).exec();
+  res.redirect('/');
 });
 
 module.exports = router;
