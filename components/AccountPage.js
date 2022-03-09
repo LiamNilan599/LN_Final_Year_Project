fb5cb76d9885f4517a8b42d72f87fbd3fc9e2002
Liam Nilan <g00366621@gmit.ie>
2022-03-09 11:10:17 +0000

    Account page for front end

--- components/AccountPage.js
+++ components/AccountPage.js
@@ -1,11 +1,23 @@
-function AccountPage() 
-{
+import Card from './layout/Card';
+import EmployeesContext from '../Store/employees-context';
+import { Textarea } from '@nextui-org/react';
+import { useContext } from 'react';
+
+function AccountPage() {
+  const settings = useContext(EmployeesContext);
+
+
   return (
     <section>
       <h1>Accounts</h1>
-      
+      <Card>
+        <Textarea
+          readOnly
+          initialValue="Almost before we knew it, we had left the ground."
+        />
+      </Card>
     </section>
   );
 }
-  
-  export default AccountPage;
+
+export default AccountPage;
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import '../global.css'
+import { NextUIProvider } from '@nextui-org/react';
 import Layout from '../components/layout/layout';
 import {EmployeesContextProvider} from '../Store/employees-context';
 
 export default function App({ Component, pageProps }) {
     return (
+        <NextUIProvider>
         <EmployeesContextProvider>
             <Layout>
                 <Component {...pageProps} />
             </Layout>
         </EmployeesContextProvider>
+        </NextUIProvider>
     )
 }
